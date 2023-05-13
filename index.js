10b9fb3d500ba30b8df1c70a5d07f0975ece6cc1
arnav09022003 <103022857+arnav09022003@users.noreply.github.com>
2023-05-13 20:45:56 +0530

    index.js

--- index.js
+++ index.js
@@ -1,5 +1,18 @@
-$(".navbar-brand").click(function(){
-    $(".navbar-brand").fadeTo("slow", 0);
-}
-);
+//---------------------------------------------NAVBAR STYLES---------------------------------------------------
+$(".brand-logo").on("mouseenter",function(){
+    $(".brand-name").removeClass("brand-hide"); 
+    $(".brand-logo").addClass("brand-hide");
+}).on("mouseleave",function(){
+    $(".brand-logo").removeClass("brand-hide"); 
+    $(".brand-name").addClass("brand-hide"); 
+});
 
+$(".navbar-toggler").on("click",function(){
+    $(".main-site,.contact-section,.contact-section-larger").toggleClass("blur");
+});
+//---------------------------------------------Contacts--------------------------------------------------------
+$(".fa-envelope").on("click",function(){
+    $("#contact").toggleClass("hide");
+    $("#contact-larger").toggleClass("hide");
+    $(".navbar").toggleClass("black-navbar");
+});
